f4979edef09693c01916b27b53e53d0db773deb7
OlexandrKostenko <106703542+OlexandrKostenko@users.noreply.github.com>
2023-02-12 21:13:18 +0200

    update

--- src/components/App.styled.jsx
+++ src/components/App.styled.jsx
@@ -2,6 +2,7 @@ import styled from "styled-components";
 
 export const Wrapper = styled.div`
         display: flex;
+        min-height: 100vh;
         flex-direction: column;
         justify-content: center;
         align-items: center;
--- src/redux/contactSlice.js
+++ src/redux/contactSlice.js
@@ -1,5 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
-import { fetchContacts } from "./operations";
+import { fetchContacts, addContact, deleteContact } from "./operations";
 
 
 
@@ -24,18 +24,34 @@ export const contactSlice = createSlice({
       state.isLoading = false;
       state.error = action.payload;
     },
+      [addContact.pending](state) {
+      state.isLoading = true;
+    },
+    [addContact.fulfilled](state, action) {
+      state.isLoading = false;
+      state.error = null;
+      state.items.push(action.payload);
+    },
+    [addContact.rejected](state, action) {
+      state.isLoading = false;
+      state.error = action.payload;
+    },
+      [deleteContact.pending](state) {
+      state.isLoading = true;
+    },
+    [deleteContact.fulfilled](state, action) {
+      state.isLoading = false;
+      state.error = null;
+      const index = state.items.findIndex(contact => contact.id === action.payload);
+      state.items.splice(index, 1);
+    },
+    [deleteContact.rejected](state, action) {
+      state.isLoading = false;
+      state.error = action.payload;
+    },
   },
 })
-    /*reducers: {
-        addContactAction: (state, action) => [...state, action.payload],
-        removeContactAction: (state, action) => {
-            const index = state.findIndex(contact => contact.id === action.payload);
-            state.splice(index, 1);
-        }
-    }
-});
 
-export const { addContactAction, removeContactAction } = contactSlice.actions;*/
 
 
 export const contactReducer = contactSlice.reducer;
