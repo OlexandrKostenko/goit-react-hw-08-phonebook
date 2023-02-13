24017abfc1e4791e0921d97ab4c5c752fda1c2ba
OlexandrKostenko <106703542+OlexandrKostenko@users.noreply.github.com>
2023-02-13 09:56:40 +0200

    fix

--- src/redux/contactSlice.js
+++ src/redux/contactSlice.js
@@ -42,7 +42,7 @@ export const contactSlice = createSlice({
     [deleteContact.fulfilled](state, action) {
       state.isLoading = false;
       state.error = null;
-      const index = state.items.findIndex(contact => contact.id === action.payload);
+      const index = state.items.findIndex(contact => contact.id === action.payload.id);
       state.items.splice(index, 1);
     },
     [deleteContact.rejected](state, action) {
